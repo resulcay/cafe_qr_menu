74e2fc412a32ad585d4614748884387ba5f30168
Resül Çay <resulcayop@gmail.com>
2023-04-27 04:32:56 +0300

    minor changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,71 +12,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: const BoxConstraints(maxWidth: 896),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Image.asset(
-                    PathService.imagePathProvider('cafe.png'),
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.only(left: 50),
-                              width: 200,
-                              height: 80,
-                              child: Row(
-                                children: [
-                                  const Text(
-                                    'cafe',
-                                    style: TextStyle(
-                                      color: ColorConstants.primaryVariant,
-                                      fontFamily:
-                                          TextConstants.secondaryFontName,
-                                      fontSize: 50,
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 896),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Image.asset(
+                      PathService.imagePathProvider('cafe.png'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.only(left: 20),
+                                width: 200,
+                                height: 80,
+                                child: Row(
+                                  children: [
+                                    const Text(
+                                      'cafe',
+                                      style: TextStyle(
+                                        color: ColorConstants.primaryVariant,
+                                        fontFamily:
+                                            TextConstants.secondaryFontName,
+                                        fontSize: 50,
+                                      ),
                                     ),
+                                    Image.asset(PathService.imagePathProvider(
+                                        'cafe_logo_transparent.png'))
+                                  ],
+                                ),
+                              ),
+                              const Spacer(),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                mainAxisSize: MainAxisSize.max,
+                                children: const [
+                                  LanguageButton(
+                                    locale: LocaleConstants.trLocale,
+                                    svgPath: 'tr.svg',
                                   ),
-                                  Image.asset(PathService.imagePathProvider(
-                                      'cafe_logo_transparent.png'))
+                                  LanguageButton(
+                                    locale: LocaleConstants.enLocale,
+                                    svgPath: 'gb.svg',
+                                  ),
+                                  LanguageButton(
+                                    locale: LocaleConstants.arLocale,
+                                    svgPath: 'ae.svg',
+                                  ),
                                 ],
                               ),
-                            ),
-                            const Spacer(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              mainAxisSize: MainAxisSize.max,
-                              children: const [
-                                LanguageButton(
-                                  locale: LocaleConstants.trLocale,
-                                  svgPath: 'tr.svg',
-                                ),
-                                LanguageButton(
-                                  locale: LocaleConstants.enLocale,
-                                  svgPath: 'gb.svg',
-                                ),
-                                LanguageButton(
-                                  locale: LocaleConstants.arLocale,
-                                  svgPath: 'ae.svg',
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
